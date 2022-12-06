92802413cb5e8f0ae16be744eaff7fa14e853e80
Simon Troadec <s2troade@gmail.com>
2022-12-06 14:32:15 +0100

    j16 p1

--- 2019/j16.py
+++ 2019/j16.py
@@ -1,28 +1,27 @@
-from collections import deque
-
 from tools.utils import List
 
 input = open("./2019/j16.txt").read().strip()
-input = "80871224585914546619083218645595"
+# input = "80871224585914546619083218645595"
 
-# input *= 10000
-offset = int(input[:7])
+basePattern = [0, 1, 0, -1]
+patterns = List()
+for i, j in enumerate(input):
+    p = List()
+    for b in basePattern:
+        for inc in range(i+1):
+            p.append(b)
+    p.rotate(1)
+    patterns.append(p)
 
-basePattern = deque([0, 1, 0, -1])
-patterns = [None for i in input]
 for phase in range(100):
     nextInput = ""
-    for index, value in enumerate(input):
-        pass
-
-    # for i in range(len(input)):
-    #     pattern = patterns[i]
-    #     nextValue = 0
-    #     for index, value in enumerate(input):
-    #         patternValue = index % len(pattern)
-    #         nextValue += int(value) * pattern[patternValue]
-    #     nextInput += str(nextValue)[-1]
-    # input = nextInput
+    for i in range(len(input)):
+        pattern = patterns[i]
+        nextValue = 0
+        for index, value in enumerate(input):
+            patternValue = index % len(pattern)
+            nextValue += int(value) * pattern[patternValue]
+        nextInput += str(nextValue)[-1]
+    input = nextInput
 
 print(input[:8])
-assert input[:8] == "24176176"
