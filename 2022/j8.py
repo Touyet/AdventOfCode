9099870e28b1d9f023a6c3f26ae1941cd257710e
Simon Troadec <s2troade@gmail.com>
2022-12-08 09:08:53 +0100

    later

--- 2022/j8.py
+++ 2022/j8.py
@@ -7,19 +7,19 @@ res = 0
 
 
 class Row:
-    def __init__(self, x, content) -> None:
+    def __init__(self, x: int, content: list[int]) -> None:
         self.x = x
         self.content = content
 
 
 class Column:
-    def __init__(self, y, content) -> None:
+    def __init__(self, y: int, content: list[int]) -> None:
         self.y = y
         self.content = content
 
 
-rows = List([Row(i, x) for i, x in enumerate(grid)])
-columns = List([Column(i, []) for i in range(len(grid[0]))])
+rows: "List[Row]" = List([Row(i, x) for i, x in enumerate(grid)])
+columns: "List[Column]" = List([Column(i, []) for i in range(len(grid[0]))])
 
 for row in rows:
     for y, c in enumerate(row.content):
@@ -27,10 +27,37 @@ for row in rows:
 
 res = 0
 for row in rows:
-    if(row.x == 0 or row.x == len(rows)-1):
-        res += 1
+    # First and last row are always visible
+    if (row.x == 0 or row.x == (len(rows)-1)):
+        res += len(row.content)
         continue
-    for column in columns:
-        if(column.y == 0 or column.y == len(rows)-1):
+    x = row.x
+
+    for y, value in enumerate(row.content):
+        column = columns[y]
+
+        # First and last column are always visible
+        if (column.y == 0 or column.y == (len(columns)-1)):
             res += 1
             continue
+
+        if (value == max(row.content) or value == max(column.content)):
+            res += 1
+            continue
+        if (value == min(row.content) or value == min(column.content)):
+            continue
+
+        left = [1 for i, j in enumerate(row.content) if (
+            j >= value and i > y)]
+        right = [1 for i, j in enumerate(row.content) if (
+            j >= value and i < y)]
+        up = [1 for i, j in enumerate(column.content) if (
+            j >= value and i > x)]
+        down = [1 for i, j in enumerate(column.content) if (
+            j >= value and i < x)]
+
+        if (len(down) == 0 or len(up) == 0 or len(left) == 0 or len(right) == 0):
+            res += 1
+            continue
+
+print(res)
