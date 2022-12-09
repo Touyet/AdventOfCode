dcc6790ff4e8aaddf03c8597f52fef507e4a828d
Simon Troadec <s2troade@gmail.com>
2022-12-09 12:26:37 +0100

    j9

--- 2022/j9.py
+++ 2022/j9.py
@@ -1,6 +1,6 @@
 from math import sqrt
 
-instructions = open("./2022/j9Test.txt").read().strip().splitlines()
+instructions = open("./2022/j9.txt").read().strip().splitlines()
 
 
 class Point:
@@ -67,14 +67,14 @@ def p1(grid: list[list[Point]], s: Point):
 def p2(grid: list[list[Point]], s: Point):
     head = s
     tails = [s for i in range(9)]
-    p1 = 1
-    for i, instruction in enumerate(instructions):
+    res: set[Point] = set([s])
+    for instruction in instructions:
         a = instruction.split(" ")
         direction = a[0]
         qty = int(a[1])
+        mx, my = move(direction)
         while qty > 0:
-            m = move(direction)
-            h = grid[head.x+m[0]][head.y+m[1]]
+            h = grid[head.x+mx][head.y+my]
 
             for i in range(len(tails)):
                 if (i == 0):
@@ -83,18 +83,20 @@ def p2(grid: list[list[Point]], s: Point):
                 tail = tails[i]
                 if (previousHead.isAdjacent(tail)):
                     break
-                dx = (previousHead.x - tail.x)//2
-                dy = (previousHead.y - tail.y)//2
+                dx = (previousHead.x - tail.x)
+                if (dx != 0):
+                    dx = dx//abs(dx)
+                dy = (previousHead.y - tail.y)
+                if (dy != 0):
+                    dy = dy//abs(dy)
                 tail = grid[tail.x + dx][tail.y+dy]
-                if (i == (len(tails)-1) and not tail.visited):
-                    grid[tail.x][tail.y].visited = True
-                    p1 += 1
-
+                if (i == (len(tails)-1)):
+                    res.add((tail.x, tail.y))
                 previousHead = tail
                 tails[i] = tail
             head = h
             qty -= 1
-    return p1
+    return len(res)
 
 
 # res = p1(grid, s)
